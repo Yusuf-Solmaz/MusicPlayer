1491d62ac23bc77bd67f42156d7012c002c33d2d
Yusuf-Solmaz <yusufmucahitsolmaz@gmail.com>
2023-05-27 21:47:59 +0300

    Button and Icons

--- lib/firstPage.dart
+++ lib/firstPage.dart
@@ -25,9 +25,9 @@ class FirstPage extends StatelessWidget {
 }
 
 class FirstPageBody extends StatelessWidget {
-
   var title = 'Podkes';
-  var firstPageText = 'A podcast is an episodic series of spoken word digital audio files that a user can download to a  personal device for easy listening.';
+  var firstPageText =
+      'A podcast is an episodic series of spoken word digital audio files that a user can download to a  personal device for easy listening.';
 
   @override
   Widget build(BuildContext context) {
@@ -39,7 +39,10 @@ class FirstPageBody extends StatelessWidget {
             width: 240,
             height: 317,
             decoration: BoxDecoration(
-              borderRadius: BorderRadius.only(topLeft: Radius.circular(90.00),topRight: Radius.circular(90.00)), // Köşeleri kavisleştirmek için kullanılır
+              borderRadius: BorderRadius.only(
+                  topLeft: Radius.circular(90.00),
+                  topRight: Radius.circular(
+                      90.00)), // Köşeleri kavisleştirmek için kullanılır
               image: DecorationImage(
                 image: AssetImage("images/img_4.png"),
                 fit: BoxFit.cover,
@@ -54,21 +57,85 @@ class FirstPageBody extends StatelessWidget {
             style: TextStyle(
               fontSize: 36,
               fontWeight: FontWeight.bold,
-
             ),
           ),
         ),
         Container(
           width: 332,
-            height: 55,
-            margin: EdgeInsets.only(top: 18),
-            child: Text(textAlign: TextAlign.center,firstPageText,style: GoogleFonts.inter(
-              fontSize: 15,
-              fontWeight: FontWeight.w300,
-            )),),
-        Container(
+          height: 55,
+          margin: EdgeInsets.only(top: 18),
+          child: Text(
+              textAlign: TextAlign.center,
+              firstPageText,
+              style: GoogleFonts.inter(
+                fontSize: 15,
+                fontWeight: FontWeight.w300,
+              )),
         ),
-        Container()
+        Container(
+          margin: EdgeInsets.only(top: 45),
+          width: 60,
+          height: 8,
+          alignment: Alignment.center,
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Container(
+                margin: EdgeInsets.only(right: 1),
+                width: 8,
+                height: 8,
+                decoration: BoxDecoration(
+                  shape: BoxShape.circle,
+                  color: Color(0xFF7B8085),
+                ),
+              ),
+              Container(
+                margin: EdgeInsets.only(left: 10),
+                width: 21,
+                height: 8,
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(10),
+                  color: Color(0xFF525298),
+                ),
+              ),
+              Container(
+                margin: EdgeInsets.only(left: 10),
+                width: 8,
+                height: 8,
+                decoration: BoxDecoration(
+                  shape: BoxShape.circle,
+                  color: Color(0xFF7B8085),
+                ),
+              ),
+            ],
+          ),
+        ),
+        Container(
+          margin: EdgeInsets.only(top: 70),
+          alignment: Alignment.center,
+          width: 70,
+          height: 70,
+          decoration: BoxDecoration(
+            borderRadius: BorderRadius.circular(20),
+            color: Colors.white,
+          ),
+          child: ElevatedButton(
+            style: ElevatedButton.styleFrom(
+              primary: Colors.transparent,
+              elevation: 0,
+              shadowColor: Colors.transparent,
+              shape: RoundedRectangleBorder(
+                borderRadius: BorderRadius.circular(35.0),
+              ),
+            ),
+            onPressed: () {},
+            child: Icon(
+              Icons.arrow_forward,
+              color: Color(0xFF525298),
+              size: 35.0,
+            ),
+          ),
+        ),
       ],
     );
   }
